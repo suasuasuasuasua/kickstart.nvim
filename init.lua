d6a8904d9ca2eb54ec14e3e4e43c8c4e0208288c
Justin Hoang <justinhoang@sua.dev>
2026-03-15 11:40:13 -0600

    fix(nvim): use simpler listchars

--- init.lua
+++ init.lua
@@ -148,7 +148,7 @@ vim.o.splitbelow = true
 --   See `:help lua-options`
 --   and `:help lua-guide-options`
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '> ', trail = '-', nbsp = '+' }
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
