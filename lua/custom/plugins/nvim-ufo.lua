be54058d7ae29602dd9b76260a8de54b75987b0c
Justin Hoang <justinhoang@sua.dev>
2026-03-22 14:58:17 -0600

    fix(ufo): add default config

--- lua/custom/plugins/nvim-ufo.lua
+++ lua/custom/plugins/nvim-ufo.lua
@@ -1,3 +1,8 @@
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
 return {
   'kevinhwang91/nvim-ufo',
   dependencies = { 'kevinhwang91/promise-async' },
@@ -8,9 +13,7 @@ return {
     -- ufo uses the same query files for folding (queries/<lang>/folds.scm)
     -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
     require('ufo').setup {
-      provider_selector = function(bufnr, filetype, buftype)
-        return { 'treesitter', 'indent' }
-      end,
+      provider_selector = function(bufnr, filetype, buftype) return { 'treesitter', 'indent' } end,
     }
   end,
 }
